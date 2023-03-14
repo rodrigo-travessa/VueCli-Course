187d457f3ea2ea1c15c2c35c3d3e1b392eec0804
Rodrigo Travessa <rodrigo.travessa@valtech.com>
2023-03-14 18:48:01 -0300

    and small intro to single file components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ny First Vue SPA with a {{ title }}</h1>
+  <br>
+  <input type="text" ref="name">
+  <br>
+  <button @click="handleClick"> Click Me</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data () {
+    return {
+      title: 'Data() title'
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -22,5 +35,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
